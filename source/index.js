24d64e18f9829811b2c8f5dfdad65b609bbe8d4a
Mehtap Tataroğlu <teytap@gmail.com>
2022-09-20 09:37:39 +0300

    pasted my project
    moved my existing weather project in index.html an index.js  file

---
+++ source/index.js
@@ -0,0 +1,84 @@
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let now = new Date();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+let currentDate = document.querySelector("h6");
+currentDate.innerHTML = `${days[now.getDay()]}  ${hour}:${minute}`;
+
+let celcius = document.querySelector("#celcius");
+let fahrenheit = document.querySelector("#fahrenheit");
+let changeDegree = document.querySelector(".changeDegree");
+
+function cel(event) {
+  event.preventDefault();
+  changeDegree.innerHTML = "24";
+}
+function fah(event) {
+  event.preventDefault();
+  changeDegree.innerHTML = "75";
+}
+celcius.addEventListener("click", cel);
+fahrenheit.addEventListener("click", fah);
+
+function newCityTemp(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${response.data.name}`;
+  let changeDegree = document.querySelector(".changeDegree");
+  let temperature = Math.round(response.data.main.temp);
+  changeDegree.innerHTML = `${temperature}`;
+  let min = document.querySelector(".min");
+  let max = document.querySelector(".max");
+  let h5 = document.querySelector("h5");
+  let minTemp = Math.round(response.data.main.temp_min);
+  let maxTemp = Math.round(response.data.main.temp_max);
+  let description = response.data.weather[0].description;
+  min.innerHTML = `${minTemp}`;
+  max.innerHTML = `${maxTemp}`;
+  h5.innerHTML = `${description}`;
+}
+function searchCity(newCityInput) {
+  let apiKey = "1223d92fc1f5a88dccf0859beb3b3425";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCityInput}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(newCityTemp);
+}
+
+function searchButtonCity(event) {
+  event.preventDefault();
+  let newCityInput = document.querySelector("#newCity").value;
+  searchCity(newCityInput);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchButtonCity);
+
+let button = document.querySelector("button");
+button.addEventListener("click", buttonEvent);
+
+function buttonEvent(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocation);
+}
+
+function getLocation(position) {
+  let apiKey = "1223d92fc1f5a88dccf0859beb3b3425";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(newCityTemp);
+}
+console.log(response.data.weather);
