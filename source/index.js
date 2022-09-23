4082f1291d7862b41b35a47bceda812ca558b3dd
Mehtap Tataroğlu <teytap@gmail.com>
2022-09-23 09:13:46 +0300

    made css changes in search engine

--- source/index.js
+++ source/index.js
@@ -38,21 +38,18 @@ fahrenheit.addEventListener("click", fah);
 
 function newCityTemp(response) {
   let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.name;
   let changeDegree = document.querySelector(".changeDegree");
+  let weatherDescription = document.querySelector(".weatherDescription");
+  let weatherHumidity = document.querySelector("#humidity");
+  let weatherWind = document.querySelector("#wind");
+  let weatherIcon = document.querySelector("#icon");
+
+  h1.innerHTML = response.data.name;
   changeDegree.innerHTML = Math.round(response.data.main.temp);
   celciusTemp = response.data.main.temp;
-  let min = document.querySelector(".min");
-  let max = document.querySelector(".max");
-  min.innerHTML = Math.round(response.data.main.temp_min);
-  max.innerHTML = Math.round(response.data.main.temp_max);
-  let weatherDescription = document.querySelector(".weatherDescription");
   weatherDescription.innerHTML = response.data.weather[0].description;
-  let weatherHumidity = document.querySelector("#humidity");
   weatherHumidity.innerHTML = response.data.main.humidity;
-  let weatherWind = document.querySelector("#wind");
   weatherWind.innerHTML = Math.round(response.data.wind.speed);
-  let weatherIcon = document.querySelector("#icon");
   weatherIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -60,6 +57,7 @@ function newCityTemp(response) {
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
   console.log(response.data.weather[0].icon);
 }
+
 function searchCity(newCityInput) {
   let apiKey = "1223d92fc1f5a88dccf0859beb3b3425";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCityInput}&appid=${apiKey}&units=metric`;
@@ -71,14 +69,6 @@ function searchButtonCity(event) {
   let newCityInput = document.querySelector("#newCity").value;
   searchCity(newCityInput);
 }
-
-let celciusTemp = null;
-
-let form = document.querySelector("form");
-form.addEventListener("submit", searchButtonCity);
-
-let button = document.querySelector("button");
-button.addEventListener("click", buttonEvent);
 
 function buttonEvent(event) {
   event.preventDefault();
@@ -92,5 +82,13 @@ function getLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(newCityTemp);
 }
+
+let celciusTemp = null;
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchButtonCity);
+
+let button = document.querySelector("button");
+button.addEventListener("click", buttonEvent);
 
 searchCity("Ankara");
